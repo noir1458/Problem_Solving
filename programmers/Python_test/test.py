e35c301809f7b44b44e4977fce4fa0d4cffc67ac
noir1458 <noir1458@gmail.com>
2022-11-21 17:00:00 +0900

    외계행성의나이.py, 진료순서정하기.py

--- programmers/Python_test/test.py
+++ programmers/Python_test/test.py
@@ -1,1 +1,16 @@
-k - n // 10
+def solution(n):
+    num = 1
+    tmp = []
+    while(True):
+        if n%num == 0:
+            tmp += [(num,n//num)]
+        if num == n:
+            break
+        num += 1
+    return len(tmp)
+
+def main():
+    print(solution(20))
+
+if __name__ == "__main__":
+    main()
