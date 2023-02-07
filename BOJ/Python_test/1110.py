2de5d42d09cec71342e73f749a9848788cf1c601
noir1458 <noir1458@gmail.com>
2023-02-07 23:46:11 +0900

    boj

--- BOJ/Python_test/1110.py
+++ BOJ/Python_test/1110.py
@@ -1,10 +1,14 @@
+import sys
+input = sys.stdin.readline
 N_tmp = input()
+if int(N_tmp) < 10:
+    N_tmp = '0' + N_tmp
 N_input = N_tmp[:2]
 count = 0
 while(True):
     N_sum = int(N_tmp[0]) + int(N_tmp[1])
-    N_tmp = N_tmp[1] + str(N_sum)[1]
+    N_tmp = N_tmp[1] + str(N_sum)[-1]
     count += 1
     if(N_input == N_tmp):
         break
-print(count)
+print(str(count))
