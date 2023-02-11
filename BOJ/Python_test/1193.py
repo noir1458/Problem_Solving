3eb7b1e0a53fa048f81164591bb51e4653619789
noir1458 <noir1458@gmail.com>
2023-02-11 21:29:15 +0900

    boj

--- BOJ/Python_test/1193.py
+++ BOJ/Python_test/1193.py
@@ -1,31 +1,15 @@
 import sys
 input = sys.stdin.readline
 input_num = int(input())
-group = 2
-start = 1
-step = 2
+group,start,step = 2,1,2
 while(True):
     if input_num <= start:
         break
-    group += 1
-    start += step
-    step += 1
-if group%2 == 0:
-    numerator = 1
-    denominator = group - 1
-    while(True):
-        if input_num == start:
-            break
-        start -= 1
-        numerator += 1
-        denominator -= 1
-else:
-    numerator = group - 1
-    denominator = 1
-    while(True):
-        if input_num == start:
-            break
-        start -= 1
-        numerator -= 1
-        denominator += 1
+    group += 1;start += step;step += 1
+numerator,denominator = 1, group - 1
+while(True):
+    if input_num == start:
+        break
+    start -= 1;numerator += 1;denominator -= 1
+if group%2==1: numerator,denominator=denominator,numerator
 print(str(numerator)+'/'+str(denominator))
