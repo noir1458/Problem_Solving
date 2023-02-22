c726f96e239b82b0087212d7df01b9a5f998b84f
noir1458 <noir1458@gmail.com>
2023-02-23 02:36:38 +0900

    boj

--- BOJ/Python_test/10811.py
+++ BOJ/Python_test/10811.py
@@ -2,6 +2,8 @@ N,M = map(int,input().split())
 result = [i for i in range(1,N+1)]
 for tmp in range(M):
     i,j = map(int,input().split())
-    for idx in range()
-
+    for cnt in range((j-i)//2):
+        result[i-1],result[j-1] = result[j-1],result[i-1]
 print(' '.join(map(str,result)))
+
+
