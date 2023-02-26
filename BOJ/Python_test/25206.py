257282d313d331a9a6061acf48543b906462b214
noir1458 <noir1458@gmail.com>
2023-02-27 00:20:09 +0900

    boj

--- BOJ/Python_test/25206.py
+++ BOJ/Python_test/25206.py
@@ -5,9 +5,8 @@ gpa_dict = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 'C+':2.5, 'C0':2.0, 'D+':1.5
 input = sys.stdin.readlines()
 for tmp in input:
     line = tmp.split()
-    sub_count += float(line[1])
-    if line[2] in '\x1a': line[2] = line[2].replace('\x1a','')
+    if '\x1a' in line[2]: line[2] = line[2].replace('\x1a','')
     if line[2] == 'P': continue
-    gpa_count += gpa_dict[line[2]]
-print(gpa_count,sub_count)
+    sub_count += float(line[1])
+    gpa_count += gpa_dict[line[2]] * float(line[1])
 print(gpa_count/sub_count)
