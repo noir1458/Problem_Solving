26a6db72cc9bbfb14ba668e065bc06b0570dc2e4
noir1458 <noir1458@gmail.com>
2024-05-10 21:40:26 +0900

    15565

--- try/15565.py
+++ try/15565.py
@@ -3,7 +3,14 @@ l = list(map(int,input().split()))
 
 
 # b개 이상의 1을 포함하는
-#가장작은 연속된 인형들의 집합의 크기
+# 가장작은 연속된 인형들의 집합의 크기
+# 그러면 양쪽에 1이면 되고
 
-
-#
+if l.count(1) < b:
+    print(-1)
+else:
+    index_l = [i for i, c in enumerate(l) if c == 1]
+    len_l = []
+    for i in range(len(index_l)-b+1):
+        len_l.append(index_l[i+b-1]-index_l[i] + 1)
+    print(min(len_l))
